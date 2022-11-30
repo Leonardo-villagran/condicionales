990bff9df56fd8b0dedc86bf02508b60fb8e65dd
Leonardo-villagran <leonardovillagran@yahoo.com>
2022-11-30 20:53:59 -0300

    html addded

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 "Trabajo número 2 (Condicionales en Javascript) de DesafioLatam para el módulo: Introducción a Javascript (G27):"
 
--Ingresar al index.html para acceder a las páginas solicitadas. 
+-Ingresar al index.html para acceder a las preguntas solicitadas. 
 
--Las páginas utilizan el mismo css y el mismo archivo js para obtener las funciones requeridas (cada pregunta utiliza una función distinta). 
+-Las páginas utilizan el mismo css (assets/css/style.css y bootstrap) y el mismo archivo js (assets/javascript/main.js) para obtener las funciones requeridas (cada pregunta utiliza una función distinta). 
 
 Acceso web desde:
 
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,68 +1,64 @@
 //Script de la pregunta 1:
 
-const borde =() =>{
+function borde() {
 
     //Selección de la foto
     let foto = document.querySelector('#foto01');
 
     //Determinar si el borde es rojo  se le quita el borde y el color.
-    
-    if(foto.style.borderColor=='red'){
+    if (foto.style.borderColor == 'red') {
         foto.style.border = "";
-    } 
-    else{
-        foto.style.border='2px solid red';
     }
-};
+    else {
+        foto.style.border = '2px solid red';
+    }
+}
 
 //Script de la pregunta 2:
 
-const verificar=() =>{
-
+function verificar() {
     //Selecionar el valor almacenado en los campos bajo las fotos.
 
-    foto02= Number(document.querySelector('#num_foto02').value);
-    foto03= Number(document.querySelector('#num_foto03').value);
-    foto04= Number(document.querySelector('#num_foto04').value);
+    let foto02 = Number(document.querySelector('#num_foto02').value);
+    let foto03 = Number(document.querySelector('#num_foto03').value);
+    let foto04 = Number(document.querySelector('#num_foto04').value);
 
     //Sumar los valores en la variable suma.
-    suma=foto02+foto03+foto04;
+    let suma = foto02 + foto03 + foto04;
 
     //Seleccionar el texto del div de id estado para cambiar su valor cuando se presione el botón. 
+    let texto = document.querySelector('#estado');
 
-    texto= document.querySelector('#estado');
-    
     //Determinar si la suma es menor o igual a 10.
-
-    if (suma<=10){
-        texto.innerHTML='<p>Has selecionado '+suma+' Stickers.</p>';
+    if (suma <= 10) {
+        texto.innerHTML = '<p>Has seleccionado ' + suma + ' stickers.</p>';
     }
-    else{
-        texto.innerHTML="<p>Llevas demasiados Stickers.</p>";
+    else {
+        texto.innerHTML = '<p>Llevas demasiados stickers. En total: ' + suma + '.</p>';
     }
-};
+}
 
 //Script de la pregunta 3:
 
-const seleccionar=() =>{
-    select01= String(document.querySelector('#select01').value);
-    select02= String(document.querySelector('#select02').value);
-    select03= String(document.querySelector('#select03').value);
+function seleccionar() {
+    let select01 = String(document.querySelector('#select01').value);
+    let select02 = String(document.querySelector('#select02').value);
+    let select03 = String(document.querySelector('#select03').value);
 
-    password=select01+select02+select03;
+    let password = select01 + select02 + select03;
 
-    texto= document.querySelector('#mensaje');
+    let texto = document.querySelector('#mensaje');
 
-    if (password=='911'){
-        texto.style.color="blue";
-        texto.innerHTML='<p>Password 1 correcto.</p>';
+    if (password == '911') {
+        texto.style.color = "blue";
+        texto.innerHTML = '<p>Password 1 correcto.</p>';
     }
-    else if (password=='714'){
-        texto.style.color="blue";
-        texto.innerHTML="<p>Password 2 correcto.</p>";
+    else if (password == '714') {
+        texto.style.color = "blue";
+        texto.innerHTML = "<p>Password 2 correcto.</p>";
     }
-    else{
-        texto.style.color="red";
-        texto.innerHTML="<p>Password incorrecto.</p>";
+    else {
+        texto.style.color = "red";
+        texto.innerHTML = "<p>Password incorrecto.</p>";
     }
-};
+}
